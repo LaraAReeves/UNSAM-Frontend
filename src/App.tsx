d58a168215736f8feea171e2ae824425fb5f046a
Agustin Narvaez <agustin.narvaez@yappa.dev>
2024-11-15 16:31:32 -0300

    Implemento VITE

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from '@mui/material'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+function App() {
+  return (
+    <>
+      <Button variant="contained" color="primary">Clickeame!!!</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material'
+
+let unsamTheme = createTheme({
+  typography: {
+    fontFamily: [
+      'Saira' /*Fuente que usa la UNSAM*/
+    ].join(','),
+  },
+  palette: {
+    primary: {
+      main:'#7DA1C4' /*Pantone 645 como indica la UNSAM, si cambia es por el color primario de la ECYT, sino no tocar.*/
+    },
+    secondary: {
+      main:'#DEECF3' /*Gris clarito complementario, puede cambiar*/
+    },
+    tonalOffset: 0.1
+  }
+})
+unsamTheme = responsiveFontSizes(unsamTheme)
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={unsamTheme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+)
