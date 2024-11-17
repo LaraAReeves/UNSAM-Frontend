6249ec88421e96d400edbb6c15b6adadfcff9bc9
franncoronel <ffranncoronel@gmail.com>
2024-11-17 13:53:20 -0300

    Extraigo componente transparente con padding variable para reducir la repetición de estilo y acortar el archivo Welcome.tsx

---
+++ src/components/ui/ContenedorTransparente.tsx
@@ -0,0 +1,18 @@
+import Box from "@mui/material/Box";
+import { ReactNode } from "react";
+
+export function ContenedorTransparente( {children, padding}: {children: ReactNode, padding:string}) {
+  return(
+    <Box
+      sx={{
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        borderRadius: 2,
+        p: {padding},
+        width: {xs:'80%', sm:'75%', md:'25%'},
+        alignSelf:'center'
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
