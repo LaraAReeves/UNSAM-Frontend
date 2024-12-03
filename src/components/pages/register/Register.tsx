493260b59ba27919f4c5abd1a2d6fe8d2f0ec2f1
franncoronel <ffranncoronel@gmail.com>
2024-12-02 21:07:56 -0300

    Mejoro mensaje de error.

--- src/components/pages/register/Register.tsx
+++ src/components/pages/register/Register.tsx
@@ -161,7 +161,7 @@ export default function Register() {
                 required: 'Debe ingresar un email',
                 pattern: {
                   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'Email inválido'
+                  message: 'Ingrese un email válido'
                 }
               }}
               render={({ field }) => (
