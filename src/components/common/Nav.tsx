f339138fdc2f3967f590c0c128c3d583b33e23c3
franncoronel <ffranncoronel@gmail.com>
2024-12-13 15:38:01 -0300

    Agrego esqueleto de navbar con botones invisibles.

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,10 +1,21 @@
-import { Typography } from "@mui/material"
-import { useNavigate } from "react-router-dom"
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material"
+// import { useNavigate } from "react-router-dom"
 
 export default function Nav() {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   return (
-    <Typography> <a onClick={() => navigate("/welcome")}>HOLIS</a> </Typography>
+    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0}} enableColorOnDark>
+      <Toolbar>
+        <IconButton /* onClick={() => navigate("/search")} */>
+        </IconButton>
+        <Box sx={{ flexGrow: 1 }}/>
+        <IconButton /* onClick={() => navigate("/")} */ >
+        </IconButton>
+        <Box sx={{ flexGrow: 1 }}/>
+        <IconButton /* onClick={() => navigate("/profile")} */ >
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   )
 }
