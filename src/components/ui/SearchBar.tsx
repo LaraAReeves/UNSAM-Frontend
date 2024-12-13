9ec2abdd0a3f91e1defca653c6cd88195b837827
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2024-12-13 17:09:57 -0300

    Maquetado de la barra de búsqueda

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,34 +1,38 @@
-import { Button, TextField, Typography } from "@mui/material"
+import { Box, IconButton, TextField} from "@mui/material"
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 export default function SeachBar() {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = () => {
-    onSearch(searchTerm);
+    //onSearch(searchTerm)
+    console.log('Realizando búsqueda:',searchTerm);
   }
 
-
   return (
-    <div className="contenedor-barra">
-    <div className="p-inputgroup">
+    <Box  sx={{ display: 'flex', alignItems: 'center', gap: 2 , mx: 2,p: 2,
+        backgroundColor: '#f0f0f0', 
+        borderRadius: '8px', 
+        boxShadow: 2,
+    }}>
       <TextField
-        hiddenLabel
+        label="Clase / Comisión / Profesor / Carrera"
+        // hiddenLabel
         id="filled-hidden-label-normal"
-        defaultValue="Normal"
-        variant="filled"
+        variant="outlined"
+        fullWidth
+        style={{width: '100%'}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleSearch}
-        size="small"
-      >
-        Buscar
-      </Button>
-    </div>
-  </div>
+      <IconButton 
+          sx={{ padding: 1,fontSize: 30 }}
+          aria-label="Buscar"
+          onClick={handleSearch}>
+       <SearchOutlinedIcon  sx={{ fontSize: 30 }}  />
+      </IconButton>
+    </Box>
   )
 
 }
