e7f0cfe96247f02072414b47e0942513c61c630b
franncoronel <ffranncoronel@gmail.com>
2024-12-13 16:04:51 -0300

    Agrego phosphor icons y cambio el color de los iconos, pero esto choca con el estilo de la página.

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,5 +1,6 @@
 import { AppBar, Box, IconButton, Toolbar } from "@mui/material"
 // import { useNavigate } from "react-router-dom"
+import { MagnifyingGlass, MapTrifold, UserCircle } from '@phosphor-icons/react'
 
 export default function Nav() {
   // const navigate = useNavigate()
@@ -7,13 +8,16 @@ export default function Nav() {
   return (
     <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0}} enableColorOnDark>
       <Toolbar>
-        <IconButton /* onClick={() => navigate("/search")} */>
+        <IconButton color="secondary" /* onClick={() => navigate("/search")} */>
+        <MagnifyingGlass size={32}/>
         </IconButton>
         <Box sx={{ flexGrow: 1 }}/>
-        <IconButton /* onClick={() => navigate("/")} */ >
+        <IconButton color="secondary" /* onClick={() => navigate("/")} */ >
+        <MapTrifold size={32}/>
         </IconButton>
         <Box sx={{ flexGrow: 1 }}/>
-        <IconButton /* onClick={() => navigate("/profile")} */ >
+        <IconButton color="secondary" /* onClick={() => navigate("/profile")} */ >
+        <UserCircle size={32}/>
         </IconButton>
       </Toolbar>
     </AppBar>
