e5f3feee212b67ea3cc2c8f0b2459562501259ef
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2024-12-15 23:21:43 -0300

    Convierto el componente  SearchBar en un componente reutilizable moviendo la lógica de búsqueda al padre

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -2,12 +2,15 @@ import { Box, IconButton, TextField} from "@mui/material"
 import { useState } from "react"
 import { MagnifyingGlass} from '@phosphor-icons/react'
 
-export default function SeachBar() {
-  const [searchTerm, setSearchTerm] = useState('');
+interface SeachBarProps {
+  onSearch: (query: string) => void
+}
+
+export default function SeachBar({ onSearch }:SeachBarProps) {
+  const [query, setQuery] = useState('')
 
   const handleSearch = () => {
-    //onSearch(searchTerm)
-    console.log('Realizando búsqueda:',searchTerm);
+    onSearch(query)
   }
 
   return (
@@ -17,8 +20,8 @@ export default function SeachBar() {
         id="filled-hidden-label-normal"
         variant="outlined"
         fullWidth
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        value={query}
+        onChange={(e) =>setQuery(e.target.value)}
         sx={{
           '& .MuiOutlinedInput-root': {borderRadius:'4px 0 0 4px',height: '56px',
           '&.Mui-focused fieldset': {borderColor: '#5f83b1'},
