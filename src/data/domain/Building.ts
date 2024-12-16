0fb4e556478cc49a4b897898e0e7b3d905e6c4e4
franncoronel <ffranncoronel@gmail.com>
2024-12-16 17:57:13 -0300

    Pensando un poco más a futuro, modelo el edificio, que podría expandirse.

---
+++ src/data/domain/Building.ts
@@ -0,0 +1,7 @@
+export class Building {
+  constructor(
+    public id: number,
+    public name: string,
+    public faculty: string, //En un futuro se podria cambiar por un objeto de tipo Faculty.
+  ){}
+}
