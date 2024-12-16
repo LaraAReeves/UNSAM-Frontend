94ce279b830aed49295bc7f8ad43610149667b3f
agusnarvaez <agus.narvaez@outlook.com>
2024-12-16 18:29:42 -0300

    Corrijo el orden del nav y sus rutas

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, IconButton, Toolbar } from "@mui/material"
+import { AppBar, IconButton, Toolbar } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import { MagnifyingGlass, MapTrifold, UserCircle } from '@phosphor-icons/react'
 
@@ -6,18 +6,16 @@ export default function Nav() {
   const navigate = useNavigate()
 
   return (
-    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }} enableColorOnDark>
-      <Toolbar>
-        <IconButton onClick={() => navigate("/profile")} aria-label='Perfil'>
-          <UserCircle size={32} alt='Usuario' />
-        </IconButton>
-        <Box sx={{ flexGrow: 1 }} />
-        <IconButton onClick={() => navigate("/")} aria-label='Inicio'>
-          <MapTrifold size={32} alt='Mapa' />
-        </IconButton>
-        <Box sx={{ flexGrow: 1 }} />
+    <AppBar position="sticky" color="primary" sx={{ top: 'auto', bottom: 0, height: '10vh', display:'flex', justifyContent:'center', alignItems:'center' }} enableColorOnDark>
+      <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
         <IconButton /* onClick={() => navigate("/search")} */ aria-label='Buscar'>
           <MagnifyingGlass size={32} alt='Lupa' />
+        </IconButton>
+        <IconButton onClick={() => navigate("/mapa/tornavias-subsuelo")} aria-label='Inicio'>
+          <MapTrifold size={32} alt='Mapa' />
+        </IconButton>
+        <IconButton onClick={() => navigate("/profile")} aria-label='Perfil'>
+          <UserCircle size={32} alt='Usuario' />
         </IconButton>
       </Toolbar>
     </AppBar>
