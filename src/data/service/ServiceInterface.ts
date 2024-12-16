c52b9db4fb90e7e55e1102256074260dfacc7b8d
franncoronel <ffranncoronel@gmail.com>
2024-12-16 17:08:22 -0300

    Cambio nombres para evitar atar el método a la consulta.

--- src/data/service/ServiceInterface.ts
+++ src/data/service/ServiceInterface.ts
@@ -1,13 +1,13 @@
 import { Entity } from "@/data/domain/Entity"
 
 export interface ServiceInterface {
-  get(): Promise<unknown>
+  getAll(): Promise<unknown>
 
   getById(id: number): Promise<unknown>
 
-  post(entity: Entity): Promise<unknown>
+  create(entity: Entity): Promise<unknown>
 
-  put(entity: Entity): Promise<unknown>
+  update(entity: Entity): Promise<unknown>
 
   delete(id: number): Promise<unknown>
 }
