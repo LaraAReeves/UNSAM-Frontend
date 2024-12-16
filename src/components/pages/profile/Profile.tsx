418bbb8faae2216f88c7d4c962bfdc117b48f6df
agusnarvaez <agus.narvaez@outlook.com>
2024-12-16 18:27:20 -0300

    Agrego main a Profile

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -3,18 +3,21 @@ import { ProfileButton } from "./ProfileButton"
 
 export default function Profile() {
   return (
-    <Stack
-      direction='column'
-      justifyContent='center'
-      alignItems='center'
-      spacing={6}
-    >
-      <Typography variant='h5' fontWeight='bold'>PERFIL</Typography>
-      <ProfileButton>Agregar Tipo de Aula</ProfileButton>
-      <ProfileButton>Editar Aula</ProfileButton>
-      <ProfileButton>Habilitar Solicitud Clase</ProfileButton>
-      <ProfileButton>Agregar Clase</ProfileButton>
-      <ProfileButton>Editar Clase</ProfileButton>
-    </Stack>
+    <main>
+      <Stack
+        direction='column'
+        justifyContent='center'
+        alignItems='center'
+        spacing={6}
+        padding={3}
+      >
+        <Typography variant='h5' fontWeight='bold'>PERFIL</Typography>
+        <ProfileButton>Agregar Tipo de Aula</ProfileButton>
+        <ProfileButton>Editar Aula</ProfileButton>
+        <ProfileButton>Habilitar Solicitud Clase</ProfileButton>
+        <ProfileButton>Agregar Clase</ProfileButton>
+        <ProfileButton>Editar Clase</ProfileButton>
+      </Stack>
+    </main>
   )
 }
