3bdaec025d3139577a8bb0751a0e322f24720ebf
franncoronel <ffranncoronel@gmail.com>
2024-12-16 16:59:41 -0300

    Implemento interfaz para los servicios.

---
+++ src/data/service/ServiceInterface.ts
@@ -0,0 +1,11 @@
+import { Entity } from "@/data/domain/Entity"
+
+export interface ServiceInterface {
+  get(): Promise<unknown>
+
+  getById(id: number): Promise<unknown>
+
+  post(entity: Entity): Promise<unknown>
+
+  put(entity: Entity): Promise<unknown>
+}
