061e7cc5713adc65d5a56f022f5e61e10796ed84
franncoronel <ffranncoronel@gmail.com>
2024-12-15 21:52:16 -0300

    Implemento la navegación hacia el perfil y de vuelta al mapa en la navbar.

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,22 +1,22 @@
 import { AppBar, Box, IconButton, Toolbar } from "@mui/material"
-// import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { MagnifyingGlass, MapTrifold, UserCircle } from '@phosphor-icons/react'
 
 export default function Nav() {
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
   return (
     <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }} enableColorOnDark>
       <Toolbar>
-        <IconButton /* onClick={() => navigate("/profile")} */ aria-label='Perfil'>
+        <IconButton onClick={() => navigate("/profile")} color="secondary" aria-label='Perfil'>
           <UserCircle size={32} alt='Usuario' />
         </IconButton>
         <Box sx={{ flexGrow: 1 }} />
-        <IconButton /* onClick={() => navigate("/")} */ aria-label='Inicio'>
+        <IconButton onClick={() => navigate("/")} color="secondary" aria-label='Inicio'>
           <MapTrifold size={32} alt='Mapa' />
         </IconButton>
         <Box sx={{ flexGrow: 1 }} />
-        <IconButton /* onClick={() => navigate("/search")} */ aria-label='Buscar'>
+        <IconButton /* onClick={() => navigate("/search")} */ color="secondary" aria-label='Buscar'>
           <MagnifyingGlass size={32} alt='Lupa' />
         </IconButton>
       </Toolbar>
