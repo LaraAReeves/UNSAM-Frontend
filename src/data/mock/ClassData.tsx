b992c4d07059b0a366c47406c379927a80e32a86
Agustin Narvaez <agustin.narvaez@yappa.dev>
2024-12-17 00:39:01 -0300

    Agrego mock de Algo3

--- src/data/mock/ClassData.tsx
+++ src/data/mock/ClassData.tsx
@@ -1,6 +1,6 @@
-export const ClassData = {
+export const casoClass = {
   className: "Conceptos de Arquitectura y Sistemas Operativos",
-  // classroomType : "Aula",
+  classroomType : "Aula",
   commission: "C-TI09",
   classroom: "A9",
   building: "Tornavias",
@@ -8,4 +8,15 @@ export const ClassData = {
   careers: ["Tecnicatura en Programación Informática", "Tecnicatura en Redes","Diagnóstico por Imágenes"],
   schedules: "08:00 - 10:00",
   viewType: "standard"
+}
+
+export const algo3Class = {
+  className: "Algoritmos 3",
+  classroomType : "Lab. de Computación",
+  commission: "C-TI10",
+  classroom: "Lab. de Computación IV",
+  building: "Tornavias",
+  teacher: ["Cosme Fulanito","Mr. X"],
+  careers: ["Tecnicatura en Programación Informática", "Tecnicatura en Redes","Ciencia de Datos","Tecnicatura en Programación Informática",],
+  schedules: "08:00 - 10:00"
 }
