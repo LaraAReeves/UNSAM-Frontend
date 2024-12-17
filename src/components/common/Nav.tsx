cd372f1436f4ad8f26888cb12a483fedeb9bba9a
Agustin Narvaez <agustin.narvaez@yappa.dev>
2024-12-17 10:42:37 -0300

    Cambio ícono si no está autenticado

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,10 +1,11 @@
 import { AppBar, IconButton, Toolbar } from "@mui/material"
 import { useNavigate } from "react-router-dom"
-import { MagnifyingGlass, MapTrifold, UserCircle } from '@phosphor-icons/react'
+import { MagnifyingGlass, MapTrifold, SignIn, UserCircle } from '@phosphor-icons/react'
+import { useAuth } from "@/context/AuthContext"
 
 export default function Nav() {
   const navigate = useNavigate()
-
+  const {isAuthenticated} = useAuth()
   return (
     <AppBar position="sticky" color="primary" sx={{ top: 'auto', bottom: 0, height: '10vh', display:'flex', justifyContent:'center', alignItems:'center' }} enableColorOnDark>
       <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
@@ -15,7 +16,8 @@ export default function Nav() {
           <MapTrifold size={32} alt='Mapa' />
         </IconButton>
         <IconButton onClick={() => navigate("/perfil")} aria-label='Perfil'>
-          <UserCircle size={32} alt='Usuario' />
+          {isAuthenticated && <UserCircle size={32} alt='Usuario' />}
+          {!isAuthenticated && <SignIn size={32} alt='Iniciar Sesión' />}
         </IconButton>
       </Toolbar>
     </AppBar>
