62a6fff641ceb5f2936f3948de51d4449459dbec
Agustin Narvaez <agustin.narvaez@yappa.dev>
2024-12-17 10:37:30 -0300

    Agrego mock de Aulas de Subsuelo

---
+++ src/data/mock/ClassRoomData.tsx
@@ -0,0 +1,96 @@
+
+export const aduitorioLecturaMundi = {
+  id: 0,
+  classroom: "Auditorio Lectura Mundi",
+  classroomType: "Auditorio",
+  building: "Tornavias",
+  level: 'Subsuelo'
+}
+
+export const lecturaMundiIdentidadVisual = {
+  id: 1,
+  classroom: "Identidad Visual",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Subsuelo'
+}
+
+export const sumSubsuelo = {
+  id: 2,
+  classroom: "SUM",
+  classroomType: "SUM",
+  building: "Tornavias",
+  level: 'Subsuelo'
+}
+
+export const a27 = {
+  id: 3,
+  classroom: "A27",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Planta Baja'
+}
+
+export const a26 = {
+  id: 4,
+  classroom: "A26",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Planta Baja'
+}
+
+export const a25 = {
+  id: 5,
+  classroom: "A25 Lab. de Artes Digitales",
+  classroomType: "Laboratorio",
+  building: "Tornavias",
+  level: 'Planta Baja'
+}
+
+export const gerenciaInformaticaSoporteTecnico = {
+  id: 6,
+  classroom: "Gerencia Informática y Soporte Técnico",
+  classroomType: "Gerencia",
+  building: "Tornavias",
+  level: 'Planta Baja'
+}
+
+export const iamk = {
+  id: 7,
+  classroom: "IAMK",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Planta Baja'
+}
+
+export const a28 = {
+  id: 8,
+  classroom: "A28",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Primer Piso'
+}
+
+export const a29 = {
+  id: 9,
+  classroom: "A29",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Primer Piso'
+}
+
+export const iamkServicios = {
+  id: 10,
+  classroom: "IAMK Servicios",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Primer Piso'
+}
+
+export const iamkDecanato = {
+  id: 11,
+  classroom: "IAMK Decanato",
+  classroomType: "Aula",
+  building: "Tornavias",
+  level: 'Primer Piso'
+}
