c20a9066f268652f03a8f6145963aee3f0ac4cd6
Agustin Narvaez <agustin.narvaez@yappa.dev>
2024-12-17 10:37:56 -0300

    Corrijo mock de Clases

--- src/data/mock/ClassData.tsx
+++ src/data/mock/ClassData.tsx
@@ -1,22 +1,55 @@
-export const casoClass = {
-  className: "Conceptos de Arquitectura y Sistemas Operativos",
+export const casoClass: IClass = {
+  id:0,
+  name: "Conceptos de Arquitectura y Sistemas Operativos",
   classroomType : "Aula",
   commission: "C-TI09",
-  classroom: "A9",
+  classroom: "A28",
+  classRoomId: 8,
   building: "Tornavias",
+  buildingLevel: 'Subsuelo',
   teacher: ["Cosme Fulanito","Mr. X"],
   careers: ["Tecnicatura en Programación Informática", "Tecnicatura en Redes","Diagnóstico por Imágenes"],
   schedules: "08:00 - 10:00",
-  viewType: "standard"
+  weekDays: ["Martes","Jueves"],
+  startDate: "2024-12-01",
+  endDate: "2025-03-01",
+  viewType: 'standard'
 }
 
-export const algo3Class = {
-  className: "Algoritmos 3",
+export const algo3Class: IClass = {
+  id:1,
+  name: "Algoritmos 3",
   classroomType : "Lab. de Computación",
   commission: "C-TI10",
-  classroom: "Lab. de Computación IV",
+  classroom: "A28",
+  classRoomId: 8,
   building: "Tornavias",
+  buildingLevel: 'Subsuelo',
   teacher: ["Cosme Fulanito","Mr. X"],
   careers: ["Tecnicatura en Programación Informática", "Tecnicatura en Redes","Ciencia de Datos","Tecnicatura en Programación Informática",],
-  schedules: "08:00 - 10:00"
+  schedules: "08:00 - 10:00",
+  weekDays: ["Lunes","Miércoles","Viernes"],
+  startDate: "2024-12-01",
+  endDate: "2025-03-01",
+  viewType: 'standard'
 }
+
+export interface IClass{
+  id: number
+  name: string
+  classroomType : string
+  commission: string
+  classroom: string
+  classRoomId: number
+  building: string
+  buildingLevel: string
+  teacher: string[]
+  careers: string[]
+  schedules: string
+  weekDays: string[]
+  startDate: string
+  endDate: string
+  viewType: string//'standard' | 'modal'
+}
+
+export const classes = [casoClass, algo3Class]
