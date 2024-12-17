d2ed3ffdb6176639282347c3914dd14226b99dd1
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2024-12-17 01:36:52 -0300

    nuevo estilo para la barra de busqueda

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, TextField} from "@mui/material"
+import { Box, Divider, IconButton, InputAdornment, TextField} from "@mui/material"
 import { useState } from "react"
 import { MagnifyingGlass} from '@phosphor-icons/react'
 
@@ -22,29 +22,30 @@ export default function SeachBar({ onSearch }:SeachBarProps) {
         fullWidth
         value={query}
         onChange={(e) =>setQuery(e.target.value)}
-        sx={{
-          '& .MuiOutlinedInput-root': {borderRadius:'4px 0 0 4px',height: '56px',
-          '&.Mui-focused fieldset': {borderColor: '#5f83b1'},
-          '& .MuiInputLabel-root': {top: '-8px'} // Ajuste de la etiqueta para que quede centrada verticalmente
-        }}}
+        sx={{ml: 1, flex: 1}}
+        slotProps={{
+          input: {
+            endAdornment: 
+            <InputAdornment position="end">
+              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+              <IconButton
+                  sx={{ padding: 1,
+                    fontSize: 32,
+                    height: '56px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                   }}
+                   color='secondary'
+                  aria-label="Buscar"
+                  onClick={handleSearch}>
+               <MagnifyingGlass size={32} alt='Lupa' color='#5f83b1'/>
+              </IconButton> 
+            </InputAdornment>,
+          },
+        }}
         aria-label='Ingresar búsqueda'
       />
-      <IconButton
-          sx={{ padding: 1,
-            fontSize: 32,
-            height: '56px',
-            backgroundColor: '#7da1c4',
-            borderRadius: '0 4px 4px 0',
-            '&:hover': {backgroundColor: '#5f83b1'},
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-           }}
-           color='secondary'
-          aria-label="Buscar"
-          onClick={handleSearch}>
-       <MagnifyingGlass size={32} alt='Lupa'/>
-      </IconButton>
     </Box>
   )
 
