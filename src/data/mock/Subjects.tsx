c75be93079d2bf9aead7dcbc25cb26bac1f386d7
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2024-12-22 13:56:46 -0300

    creo mock de asignaturas

---
+++ src/data/mock/Subjects.tsx
@@ -0,0 +1,32 @@
+export const Subjects = {
+  tpi:[ 'Matemática I',
+        'Laboratorio de Computación I',
+        'Electricidad y Magnetismo',
+        'Laboratorio de Computación II',
+        'Sistemas de Procesamiento de Datos',
+        'Matemática II',
+        'Algoritmos I',
+        'Matemática III',
+        'Conceptos de Arquitecturas y Sistemas Operativos',
+        'Algoritmos II',
+        'Redes Locales',
+        'Métodos Numéricos',
+        'Algoritmos III',
+        'Bases de Datos',
+        'Seminario de  Programación Concurrente, Paralela y Distribuida',
+        'Programación con Herramientas Modernas',
+        'Proyectos de Software',
+        'Paradigmas de Programación'
+      ],
+  redes:[//Comparten las mismas materias hasta caso
+          'Redes de Información I',
+          'Redes Locales',
+          'Proyecto I',
+          'Redes de Información II',
+          'BAdministración de Redes de Computadoras',
+          'Proyecto II',
+          'Redes de Información III',
+          'Sistemas Avanzados de Comunicación',
+          'Proyecto III'
+        ]
+}
