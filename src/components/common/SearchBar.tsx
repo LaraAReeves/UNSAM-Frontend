dd353a34016786ba392add7075504d32ea09324e
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2024-12-23 03:16:33 -0300

    Se agrego el componente de autocompletado en la barra de búsqueda

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,6 +1,7 @@
-import { Box, Divider, IconButton, InputAdornment, TextField} from "@mui/material"
+import { Autocomplete, Box, Divider, IconButton, InputAdornment, TextField} from "@mui/material"
 import { useState } from "react"
 import { MagnifyingGlass} from '@phosphor-icons/react'
+import subjects from "@/data/mock/Subjects"
 
 interface SeachBarProps {
   onSearch: (query: string) => void
@@ -15,36 +16,42 @@ export default function SeachBar({ onSearch }:SeachBarProps) {
 
   return (
     <Box  sx={{ display: 'flex', alignItems: 'center', mx: 2,p: 2,}}>
-      <TextField
-        label='Clase / Comisión / Profesor / Carrera'
+      <Autocomplete
+        freeSolo
         id="filled-hidden-label-normal"
-        variant="outlined"
+        disableClearable
         fullWidth
-        value={query}
-        onChange={(e) =>setQuery(e.target.value)}
-        sx={{ml: 1, flex: 1}}
-        slotProps={{
-          input: {
-            endAdornment: 
-            <InputAdornment position="end">
-              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-              <IconButton
-                  sx={{ padding: 1,
-                    fontSize: 32,
-                    height: '56px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                   }}
-                   color='secondary'
-                  aria-label="Buscar"
-                  onClick={handleSearch}>
-               <MagnifyingGlass size={32} alt='Lupa' color='#5f83b1'/>
-              </IconButton> 
-            </InputAdornment>,
-          },
-        }}
-        aria-label='Ingresar búsqueda'
+        options={subjects.map((option) => option.subject)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label='Clase / Comisión / Profesor / Carrera'
+            slotProps={{
+              input: {
+                ...params.InputProps,
+                type: 'search',
+                endAdornment: 
+                  <InputAdornment position="end">
+                    <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                    <IconButton
+                        sx={{ padding: 1,
+                          fontSize: 32,
+                          height: '56px',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}
+                        color='secondary'
+                        aria-label="Buscar"
+                        onClick={handleSearch}>
+                    <MagnifyingGlass size={32} alt='Lupa' color='#5f83b1'/>
+                    </IconButton> 
+                  </InputAdornment>
+              },
+            }}
+            aria-label='Ingresar búsqueda'
+          />
+        )}
       />
     </Box>
   )
